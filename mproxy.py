65e932d7be3ce4e2bc9e232d5a8b3cbc7e157f87
Alok <alokg@me.com>
2017-03-17 23:43:41 -0700

    done

--- mproxy.py
+++ mproxy.py
@@ -6,6 +6,12 @@ import ssl
 from thread import *
 import pdb
 import os
+import datetime
+
+
+# Globals
+log_dir = None
+request_num = 0
 
 def validate_port(port):
     if not port:
@@ -28,29 +34,16 @@ def parse_input_args():
 
     print args
     validate_port(args.port)
+    log = str(args.log)
+    if not os.path.exists(log):
+        print "ERROR: log directory must already exist"
+        exit()
 
-    logdir = args.log
-    #
-    # if logdir:
-    #     if not os.path.exists(logdir):
-    #         try:
-    #             os.makedirs(logdir)
-    #         except OSError as ex:
-    #             if ex.errno != errno.EEXIST:
-    #                 raise
-    #     if not logdir.endswith("/"):
-    #         logdir = logdir + "/"
-    # if not os.path.exists(CERTS_DIR):
-    #     try:
-    #         os.makedirs(CERTS_DIR)
-    #     except OSError as ex:
-    #         if ex.errno != errno.EEXIST:
-    #             raise
-
-    return args.port, args.numworker, args.timeout, logdir
+    return args.port, args.numworker, args.timeout, log
 
 
-def proxy_server(webserver, port, conn, data, addr):
+def proxy_server(webserver, port, conn, data, addr, filename):
+    print "FILENAME:", filename
     buffer_size = 4096
     try:
 
@@ -59,6 +52,10 @@ def proxy_server(webserver, port, conn, data, addr):
         print "------------------DATA SENT FROM PROXY TO SERVER------------------"
         print data
         s.send(data)
+        if filename:
+            with open(filename, 'a+') as f:
+                f.write(data + "\n")
+
 
         while 1:
             # read reply or data to from end web server
@@ -72,6 +69,9 @@ def proxy_server(webserver, port, conn, data, addr):
                 dar = "%.3s" % (str(dar))
                 dar = "%s KB" % (dar)
                 print "Request Done: %s => %s <=" % (str(addr[0]), str(dar))
+                if filename:
+                    with open(filename, 'a+') as f:
+                        f.write(reply + "\n")
             else:
                 break
         s.close()
@@ -123,37 +123,6 @@ def sanitize_data(data, webserver):
     # print data
     return data
 
-def is_clienthello(self, data):
-        if len(data) < 20:
-            return False
-        if data.startswith('\x16\x03'):
-            length, = struct.unpack('>h', data[3:5])
-            return len(data) == 5 + length
-        elif data[0] == '\x80' and data[2:4] == '\x01\x03':
-            return len(data) == 2 + ord(data[1])
-        else:
-            return False
-
-def extract_sni_name(self, packet):
-    if packet.startswith('\x16\x03'):
-        stream = io.BytesIO(packet)
-        stream.read(0x2b)
-        session_id_length = ord(stream.read(1))
-        stream.read(session_id_length)
-        cipher_suites_length, = struct.unpack('>h', stream.read(2))
-        stream.read(cipher_suites_length + 2)
-        extensions_length, = struct.unpack('>h', stream.read(2))
-        while True:
-            data = stream.read(2)
-            if not data:
-                break
-            etype, = struct.unpack('>h', data)
-            elen, = struct.unpack('>h', stream.read(2))
-            edata = stream.read(elen)
-            if etype == 0:
-                server_name = edata[5:]
-                return server_name
-
 
 def https_proxy_server(port, conn, data, addr, webserver):
     conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
@@ -163,29 +132,29 @@ def https_proxy_server(port, conn, data, addr, webserver):
     ssl_client_socket = client_context.wrap_socket(conn, server_side=True)
     print "SSL Socket - Successfully Wrapped"
 
-    getreq = ssl_client_socket.read(4096)
+    ssl_res = ssl_client_socket.read(4096)
     print "read from client socket - success"
-    lines = getreq.split("\n")
+    lines = ssl_res.split("\n")
     lines = [x for x in lines if not x.startswith('Accept-Encoding:')]
     conn_ind = lines.index("Connection: keep-alive\r")
     if conn_ind != -1:
         lines[conn_ind] = "Connection: close\r"
-    getreq = '\n'.join(lines)
-    print getreq
+    ssl_res = '\n'.join(lines)
+    print ssl_res
     server_context = ssl.create_default_context()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     print "Wrapping socket"
-    ssl_server_socket = server_context.wrap_socket(server_socket, server_hostname='yelp.com')
+    ssl_server_socket = server_context.wrap_socket(server_socket, server_hostname=webserver)
     print "Pre-Connect"
     ssl_server_socket.connect((webserver, int(port)))
-    print "CONNECTED BISH"
+    print "CONNECTED"
     print "------------------------------------------------------"
 
 
 
 
-def conn_string(conn, data, addr):
+def parse_req(conn, data, addr):
     # Client Browser requests
     try:
         first_line = data.split('\n')[0]
@@ -199,26 +168,34 @@ def conn_string(conn, data, addr):
         if webserver_pos == -1:
             webserver_pos = len(temp)
         webserver = ""
-
+        is_https_request = is_ssl_req(data)
         port = -1
         if port_pos == -1 or webserver_pos < port_pos:
             port = 80
             webserver = temp[:webserver_pos]
         else:
-            port = int(temp[(port_pos + 1):])
+            port = int(temp[(port_pos + 1):]) if is_https_request else int((temp[(port_pos + 1):])[webserver_pos - port_pos - 1])
             webserver = temp[:port_pos]
+
         print "------------------------------------------------------"
         print "WEBSERVER:", webserver
-        is_https_request = is_ssl_req(data)
+        file_name = None
+
+        addr_str = str(addr[0])
+
+        if len(log_dir) > 0:
+            file_name = str(log_dir) + str(request_num) + '_' + addr_str + '_' + str(webserver)
+
         if is_https_request:
             https_proxy_server(port, conn, data, addr, webserver)
         else:
             data = sanitize_data(data, webserver)
-            proxy_server(webserver, port, conn, data, addr)
+            proxy_server(webserver, port, conn, data, addr, file_name)
     except:
         pass
 
 def accept_conn(s):
+    global request_num
     buffer_size = 4096
     while True:
         try:
@@ -226,11 +203,11 @@ def accept_conn(s):
             data = conn.recv(buffer_size)
             print "------------------DATA FROM CLIENT TO FORWARD TO SERVER------------------"
             print data
-
-            start_new_thread(conn_string, (conn, data, addr))
+            request_num += 1
+            start_new_thread(parse_req, (conn, data, addr))
         except:
             s.close()
-            print "Proxy server shutting down...."
+            print "Shutting proxy server"
             sys.exit(1)
 
 def connect_socket(num_workers, port):
@@ -246,7 +223,11 @@ def connect_socket(num_workers, port):
     accept_conn(s)
 
 def main():
+    global log_dir
     port, num_workers, timeout, log_dir = parse_input_args()
+    print "log_dir:",log_dir
+    if not log_dir.endswith('/'):
+        log_dir = log_dir + '/'
     connect_socket(num_workers, port)
 
 if __name__ == '__main__':
